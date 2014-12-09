076b7116f417577b6694f56f7e665d0199b085f7
kenokabe <kenokabe@gmail.com>
2014-12-10 00:25:45 +0900

    commit

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ console.log(atom);
 
 var proc = require('child_process');
 
-var jsbin = proc.fork('./node_modules/jsbin/bin/jsbin');
-var app1 = proc.spawn(atom, ['app1.js']);
+var jsbin = proc.fork(__dirname + '/node_modules/jsbin/bin/jsbin');
+var app1 = proc.spawn(atom, [__dirname + '/app1.js']);
 
 app1.on('close', function(data)
 {
